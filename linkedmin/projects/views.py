679ca35f0653acdaecf2c7e8e43d623deaf5cc7b
Kateschka <71079968+kateschka@users.noreply.github.com>
2023-04-26 21:07:56 +0100

    project created

---
+++ linkedmin/projects/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Project
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()
+    context = {'project': project,
+               'tags': tags}
+    return render(request, 'projects/single-project.html', context)
