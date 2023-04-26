abf175242a6bebaf8aeee1b21416fbae2b933205
Kateschka <71079968+kateschka@users.noreply.github.com>
2023-04-26 21:12:42 +0100

    edited settings

--- linkedmin/linkedmin/settings.py
+++ linkedmin/linkedmin/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5+gnki37(u(i!3$oa9-0#1i6=ieqp81728ww^_--%0g=j7tva0'
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
